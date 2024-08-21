9e8c8d47ec6561acc59c2127dba54aacba8a4157
vasu <vasuantala8283@gmail.com>
2024-08-21 14:43:03 +0530

    a

--- src/controller/blog/read.ts
+++ src/controller/blog/read.ts
@@ -7,7 +7,7 @@ export const Data = async (req: Request, res: Response) => {
     let user = req.body;
 
     let Data = await Model.blog.find({
-        category: new ObjectId("66c45b881e842a8382992f6b")
+        category: new ObjectId("66c45b881e842a8382992f6b")  //music category
     }).sort({ createdAt: 1 });
 
     console.log(Data)
